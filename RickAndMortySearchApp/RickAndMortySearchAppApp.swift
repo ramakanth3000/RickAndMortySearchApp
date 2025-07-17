e7e851aa230e1633655334f769ba91218cf3adb5
Ram Chennupati <ram.chennupati@poppulo.com>
2025-07-17 11:39:44 -0600

    Initial Commit

---
+++ RickAndMortySearchApp/RickAndMortySearchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortySearchAppApp.swift
+//  RickAndMortySearchApp
+//
+//  Created by Ram Chennupati on 7/17/25.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortySearchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
